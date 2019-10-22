23f1cd1ce26c9a24760f9e0c91dd340f09184b3a
an pham <vincentphampham1993@gmail.com>
2019-10-21 21:18:51 -0400

    update computer_specs.sh

--- computer_specs_analyzer.sh
+++ computer_specs_analyzer.sh
@@ -1,21 +1,11 @@
 #!/bin/bash
 # Description: the template and outline of the this scripted was taken from treehouses team. I did not come up with design. To know more about the team project please visit https://treehouses.io  The lib and commands are rewritten wto fit my own need.
 
-set n # syntax check
-
-library_path=modules
+set -e
+library_path='modules'
 # extracting library
 
 
-function read_file_to_array {
-i=1
-while read line
-do 
-	array[$i]="$line"
-	(( i++ ))
- 
-done 
-}
 
 
 [[ ! `whoami` == root ]] && echo 'error: require root privellege'
@@ -29,13 +19,16 @@ function toolbox_supported_command {
 }
 
 
-
+echo "the argument is $1"
 case $1 in 
 	'ram')
-	  ram_report 
+		ram_report 
     ;; 
-  'list_function')
+  'list')
 		toolbox_supported_command "$library_path"
 	  ;;	
+	 *)
+		echo 'error: this argument is not supported'
+    ;;
 esac
   
