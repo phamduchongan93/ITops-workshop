81a96b48dab5b02ac79192cf0fece764802de521
an pham <vincentphampham1993@gmail.com>
2019-10-21 20:55:18 -0400

    update computer_specs_anayler.sh

--- computer_specs_analyzer.sh
+++ computer_specs_analyzer.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 # Description: the template and outline of the this scripted was taken from treehouses team. I did not come up with design. To know more about the team project please visit https://treehouses.io  The lib and commands are rewritten wto fit my own need.
 
-
+set n # syntax check
 
 library_path=modules
 # extracting library
 
 
-read_file_to_array {
+function read_file_to_array {
 i=1
 while read line
 do 
@@ -24,17 +24,18 @@ done
 
 function toolbox_supported_command {
   # importing modules filename  to an array
-  array=(`ls  "$library_path"` | xargs)
+  array=(`ls  "$1" | xargs`)
   echo ${array[@]}
 }
 
 
-function help {
-}
 
 case $1 in 
-  f
-    ram_report 
-  ;; 
+	'ram')
+	  ram_report 
+    ;; 
+  'list_function')
+		toolbox_supported_command "$library_path"
+	  ;;	
 esac
   
